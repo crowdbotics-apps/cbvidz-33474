ade894b2fb26d851b18812061b25dafaf8ae7e46
itech@cyberservices.com <itech@cyberservices.com>
2022-02-09 23:15:20 +0100

    SMTP

--- cbvidz_33474/settings.py
+++ cbvidz_33474/settings.py
@@ -198,10 +198,14 @@ LOGOUT_REDIRECT_URL = 'home'
 ADMIN_URL=env.str("ADMIN_URL", "")
 YOUTUBE_API_KEY=env.str("YOUTUBE_API_KEY", "")
 
-EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
-EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
-EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
-EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
