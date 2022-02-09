4fc438568b237601308916a9f34c1ded87f2601d
itech@cyberservices.com <itech@cyberservices.com>
2022-02-09 19:05:52 +0100

    dash

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbvidz-33474.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbvidz_33474.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
