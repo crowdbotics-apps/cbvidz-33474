d7dd4b3801b5abf7bc5a23186cf642fc36367e2e
itech@cyberservices.com <itech@cyberservices.com>
2022-02-09 20:49:24 +0100

    strURL

--- cbvidz_33474/urls.py
+++ cbvidz_33474/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 admin_url = os.getenv('ADMIN_URL')
 urlpatterns = [
     path("", include("home.urls")),
-    path(admin_url + '/admin/', admin.site.urls),
+    path(str(admin_url) + '/admin/', admin.site.urls),
 ]
 
 admin.site.site_header = "cbvidz"
